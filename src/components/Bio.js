789e25f91d473730d9e9f851480495055b7751ef
srmward <srmward@gmail.com>
2018-12-01 22:53:35 -0800

    First Commit, Initial Setup

---
+++ src/components/Bio.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <p>
+          a blog about javascript, node, and other code musings.
+          <br />
+          <a href="https://twitter.com/alephnode">twitter plug</a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/utils/typography.js
@@ -0,0 +1,23 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.45,
+  headerFontFamily: [
+    'Roboto Mono',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+  ],
+  bodyFontFamily: ['Roboto Mono', 'sans-serif'],
+})
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles()
+}
+
+export default typography
+export const rhythm = typography.rhythm
+export const scale = typography.scale
