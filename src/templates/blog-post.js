306b7c74ddd930385d354cc0443d42bc74caa215
srmward <srmward@gmail.com>
2019-02-13 22:00:51 -0800

    Add Style so It Makes it to Individual Pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import '../styles/global.css'
 import { Link, graphql } from 'gatsby'
 import EmailForm from '../components/EmailForm'
 
